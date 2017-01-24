d4446c1360cf33b692272100b757b2194d9d3a3e
mundomon <dedam@mac-mini-de-dedam-6.local>
2017-01-24 20:08:03 +0100

    Initial Commit

---
+++ Entrega4Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Entrega4Chat
+//
+//  Created by dedam on 24/1/17.
+//  Copyright © 2017 dedam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Entrega4Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entrega4Chat
+//
+//  Created by dedam on 24/1/17.
+//  Copyright © 2017 dedam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
